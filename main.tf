5bb5bfbb1e0eec963def608a23c8529aa590f198
Aslam Mohammad <118258858+AslamMohammad11@users.noreply.github.com>
2025-08-11 19:57:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,72 @@ provider "aws" {
 }
 
 # ---------------------------
+# VPC
+# ---------------------------
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "eks_public_subnet_a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-public-a"
+  }
+}
+
+resource "aws_subnet" "eks_public_subnet_b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-public-b"
+  }
+}
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+resource "aws_route_table" "eks_public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "eks_rta_a" {
+  subnet_id      = aws_subnet.eks_public_subnet_a.id
+  route_table_id = aws_route_table.eks_public_rt.id
+}
+
+resource "aws_route_table_association" "eks_rta_b" {
+  subnet_id      = aws_subnet.eks_public_subnet_b.id
+  route_table_id = aws_route_table.eks_public_rt.id
+}
+
+# ---------------------------
 # IAM Role for EKS Cluster
 # ---------------------------
 resource "aws_iam_role" "eks_cluster_role" {
@@ -55,15 +121,17 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
 }
 
 # ---------------------------
-# EKS Cluster (using your VPC & Subnet)
+# EKS Cluster
 # ---------------------------
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = [aws_subnet.mysubnet_public_1.id]  # your existing public subnet
-    security_group_ids = [aws_security_group.mysecuritygroup.id] # your SG
+    subnet_ids = [
+      aws_subnet.eks_public_subnet_a.id,
+      aws_subnet.eks_public_subnet_b.id
+    ]
   }
 
   depends_on = [
@@ -78,7 +146,10 @@ resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.mysubnet_public_1.id]
+  subnet_ids      = [
+    aws_subnet.eks_public_subnet_a.id,
+    aws_subnet.eks_public_subnet_b.id
+  ]
 
   scaling_config {
     desired_size = 2
